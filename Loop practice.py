fbc8a66312f43b55788083c3c690d48714097704
giehxpose <giehxpose@gmail.com>
2021-02-05 17:31:11 +0700

    -add new file

---
+++ Loop practice.py
@@ -0,0 +1,9 @@
+print('Loop1')
+terpukul=4
+for fap in range(0, terpukul):
+    print(f'I fap {fap} times a day')
+
+print('\nLoop2')
+play=['abaddon','io','ES']
+for p in range(0,len(play)):
+    print(f'{p+1}. I like {play[p]}')
