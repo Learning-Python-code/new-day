3a69401ba508ab4de5002a2fc5d14fc7506bb843
giehxpose <giehxpose@gmail.com>
2021-02-05 19:50:38 +0700

    -add new dictionary practice

---
+++ Dict_practice.py
@@ -0,0 +1,18 @@
+# Dictionary practice
+Data_MCS = {
+    'PS': [{'Nama': 'Adnan', 'Tinggal': 'Mess'},
+           {'Nama': 'Yoga', 'Tinggal': 'SKML'},
+           {'Nama': 'Rezky', 'Tinggal': 'SKML'},
+           {'Nama': 'Suriansyah', 'Tinggal': 'Mess'},
+           {'Nama': 'Edwin', 'Tinggal': 'Kos'},
+           {'Nama': 'Dimas', 'Tinggal': 'Rumah'}],
+    'Teknisi': [{'Nama': 'Jay', 'Tinggal': 'M1'},
+                {'Nama': 'Diki', 'Tinggal': 'M1'},
+                {'Nama': 'Ogi', 'Tinggal': 'Rumah'},
+                {'Nama': 'Fikri', 'Tinggal': 'Kos'},
+                {'Nama': 'Rangga', 'Tinggal': 'M1'},
+                {'Nama': 'Gofur', 'Tinggal': 'M1'}]
+
+}
+
+print(f"{Data_MCS['PS'][4]['Nama']} Bisa berangkat")
